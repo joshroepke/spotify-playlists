29f19a2fac7354c2d4abf3f5544aa1a6b0d50bc0
Josh Roepke <roepkejh@gmail.com>
2021-08-12 23:13:28 -0400

    add playlist url to output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 # output "tracks" {
 #   value = data.spotify_search_track.artist_4.tracks
 # }
+
+
+output "playlist_url" {
+  value       = "https://open.spotify.com/playlist/${spotify_playlist.playlist.id}"
+  description = "Visit this URL in your browser to listen to the playlist"
+}
